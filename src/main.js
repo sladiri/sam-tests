c4bb852d7a0ed5e28f4b0388dd3a46ae80ea046a
Slađan Ristić <sladiri@users.noreply.github.com>
2016-11-01 17:28:05 +0100

    Refactor test code.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import yo from 'yo-yo'
-import mainStyles from './styles/main.css'
-import secondStyles from './styles/second.css'
+import { first as oneStyle } from './styles/first.css'
+import { second as twoStyle } from './styles/second.css'
 
-console.log('foo', mainStyles, secondStyles)
 
-document.body.appendChild(yo`<div class="${mainStyles.world}">Hello</div>`)
-
+document.body.appendChild(yo`<div>Hello</div>`)
 const { map } = Array.prototype
 document.querySelectorAll('div')
-  ::map(node => do { node.innerHTML = node.innerHTML + ' World!' })
-
-document.body.appendChild(yo`<div class="${secondStyles.hello}">A Test</div>`)
+  ::map(node => do {
+    node.innerHTML = node.innerHTML + ' World!'
+    node.className = oneStyle
+  })
+document.body.appendChild(yo`<div class="${twoStyle}">A Test</div>`)
