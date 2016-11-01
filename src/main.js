971e287d07410d11cc62d4dcdfabc1401120b66f
Slađan Ristić <sladiri@users.noreply.github.com>
2016-11-01 23:52:45 +0100

    Add SAM counter example with action "hang back".

--- src/main.js
+++ src/main.js
@@ -1,13 +1,62 @@
 import yo from 'yo-yo'
-import { first as oneStyle } from './styles/first.css'
-import { second as twoStyle } from './styles/second.css'
+import style from './main.css'
 
-
-document.body.appendChild(yo`<div>Hello</div>`)
-const { map } = Array.prototype
-document.querySelectorAll('div')
-  ::map(node => do {
-    node.innerHTML = node.innerHTML + ' World!'
-    node.className = oneStyle
-  })
-document.body.appendChild(yo`<div class="${twoStyle}">A Test</div>`)
+const model = {
+  field: 0,
+  accept ({ value }) {
+    if (value === null) {
+      this.field = 0
+    } else {
+      this.field += 1
+    }
+    state(model)
+  },
+}
+const state = (model) => {
+  // forbid increment
+  const action = () => actions.increment()
+  const disabled = model.field >= 5
+  actions.blockIncrement = disabled
+  const nextView = view({ counter: model.field, action, disabled })
+  nap({ model })
+  render({ nextView }) // TODO: NAP first?
+}
+const nap = ({ model }) => {
+  if (model.field >= 5) {
+    actions.reset()
+  }
+}
+const actions = {
+  blockIncrement: null,
+  increment () {
+    setTimeout(() => {
+      if (!this.blockIncrement) {
+        model.accept({})
+      } else {
+      }
+    }, 1000)
+  },
+  reset () {
+    setTimeout(() => {
+      model.accept({ value: null })
+    }, 3000)
+  },
+}
+const view = ({ counter, action, disabled }) => {
+  return yo`
+    <div class="${style[`counter${counter}`]}">
+      ${counter}
+      <br />
+      <button disabled=${disabled} onclick=${action}>increment</button>
+    </div>`
+}
+const render = ({ nextView }) => {
+  if (!initialView) {
+    initialView = nextView
+    document.body.appendChild(initialView)
+  } else {
+    yo.update(initialView, nextView)
+  }
+}
+let initialView
+state(model)
